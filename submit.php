4ff5bb9049dc08d7b3e2429877fa3a9bd8a54912
Sagar Bansal <sagarbansal099@gmail.com>
2019-01-03 16:55:00 +0000

    email

--- submit.php
+++ submit.php
@@ -4,32 +4,115 @@
 if( $_SERVER['REQUEST_METHOD'] == 'POST'){
 
     $Name = $_POST['Name'];
-$CollegeCity = $_POST['CollegeCity'];
-$CollegeName = $_POST['CollegeName'];
-$CollegeStrength = $_POST['CollegeStrength'];
-$CollegeAddress = $_POST['CollegeAddress'];
-$CollegePincode = $_POST['CollegePincode'];
-$CurrentAddress = $_POST['CurrentAddress'];
-$CurrentPincode = $_POST['CurrentPincode'];
-$MobileNumber = $_POST['MobileNumber'];
-$WhatsappNumber = $_POST['WhatsappNumber'];
-$Email = $_POST['Email'];
-$Motive = $_POST['Motive'];
-$Experience = $_POST['Experience'];
-$skills = $_POST['skills'];
-$Approach = $_POST['Approach'];
-$Requirements = $_POST['Requirements'];
+    $CollegeCity = $_POST['CollegeCity'];
+    $CollegeName = $_POST['CollegeName'];
+    $CollegeStrength = $_POST['CollegeStrength'];
+    $CollegeAddress = $_POST['CollegeAddress'];
+    $CollegePincode = $_POST['CollegePincode'];
+    $CurrentAddress = $_POST['CurrentAddress'];
+    $CurrentPincode = $_POST['CurrentPincode'];
+    $MobileNumber = $_POST['MobileNumber'];
+    $WhatsappNumber = $_POST['WhatsappNumber'];
+    $Email = $_POST['Email'];
+    $Motive = $_POST['Motive'];
+    $Experience = $_POST['Experience'];
+    $skills = $_POST['skills'];
+    $Approach = $_POST['Approach'];
+    $Requirements = $_POST['Requirements'];
 
-$con=mysqli_connect("localhost:3306","vipul3","Ecellvnit123@","CampusAmbassador");
-// Check connection
-if (mysqli_connect_errno()){
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-$skills_string = implode(', ', $_POST['skills']);
-$query = mysqli_query($con,"insert into campus (Name,`College City`,`College Name`,`College Strength`,`College Address`,`College Pincode`,`Current Address`,`Current Pincode`,`Mobile Number`,`Whatsapp Number`,Email,Motive,Experience,Skills,Approach,Arrangements)
-                              values('$Name','$CollegeCity','$CollegeName','$CollegeStrength','$CollegeAddress','$CollegePincode','$CurrentAddress','$CurrentPincode','$MobileNumber','$WhatsappNumber','$Email','$Motive','$Experience','$skills_string','$Approach','$Requirements')");
+    error_reporting(E_ERROR | E_PARSE);
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+    try{
+        $con=mysqli_connect("localhost:3306","vipul3","Ecellvnit123@","CampusAmbassador");
+    }
+    catch(MySQLi_Sql_Exception $ex){
+
+            echo("<p>Error in connecting</p>");
+
+        }
 
 
+
+
+    // Check connection
+    if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    $skills_string = implode(', ', $_POST['skills']);
+
+    try{
+        $query = mysqli_query($con,"insert into campus (Name,`College City`,`College Name`,`College Strength`,`College Address`,`College Pincode`,`Current Address`,`Current Pincode`,`Mobile Number`,`Whatsapp Number`,Email,Motive,Experience,Skills,Approach,Arrangements)
+                                  values('$Name','$CollegeCity','$CollegeName','$CollegeStrength','$CollegeAddress','$CollegePincode','$CurrentAddress','$CurrentPincode','$MobileNumber','$WhatsappNumber','$Email','$Motive','$Experience','$skills_string','$Approach','$Requirements')");
+
+    }
+    catch(MySQLi_Sql_Exception $ex){
+            echo("<p>Error in connecting</p>");
+
+        }
+
+
+
+    // Mailing
+    require_once "Mail.php";
+    $from = "E-CELL VNIT <noreply@ecellvnit.org>";    //your mail id
+    $to = $Email;
+    $subject = "Registration successful";
+    $body = '
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <style>
+                li{
+                    padding:10px;
+                }
+                p{
+                    font-size:16px;
+                }
+            </style>
+        </head>
+        <body style="width:100%; background-color:#fff; padding:50px 30px; color:1e1e1e; margin-top:100px;font-family:Helvetica,Arial,sans-serif">
+        <div style="background-color:#000;width:90%;  padding:10px 30px;"><img src="https://www.ecellvnit.org/img/logo-ecell.png"></div>
+            <div style="width:90%; background-color:#f7f9fb; padding:50px 30px;color: #212121;">
+                <h3><b>Hello '.$Name.',</b></h3>
+                <p style="font-size:18px;">Thank You for joining campus ambassador program. All the important details will be sent to you very shortly. <b></b><br></p>
+
+            </div>
+
+            <hr>
+            <div style="padding:50px 30px; width:90%;color:#fff; background-color:#1e1e1e;">
+                <h5>Contact Us</h5>
+                <p>Anushree Rungta<br>
+                (Core Coordinator)</p>
+                <p>+91 8830 431811</p>
+                <p>or</p>
+                <p>Mail Us: contact@ecellvnit.org</p>
+
+            </div>
+
+        </body>
+    </html>';
+
+    $host = "ssl://sharedlinux.cloudhostdns.net";
+    $port = "465";
+    $username = "noreply@ecellvnit.org";          //your mail id
+    $password = "VNIT@123";                      //password of this mail id
+
+    $headers = array('MIME-Version' => '1.0rn',
+        'Content-Type' => "text/html; charset=ISO-8859-1rn",
+        'From' => $from,
+        'To' => $to,
+        'Subject' => $subject);
+    $smtp = Mail::factory('smtp',
+        array('host' => $host,
+            'port' => $port,
+            'auth' => true,
+            'username' => $username,
+            'password' => $password));
+
+
+
+    $mail = $smtp->send($to, $headers, $body);
 
 
 
@@ -50,7 +133,8 @@ $query = mysqli_query($con,"insert into campus (Name,`College City`,`College Nam
   </title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Material+Icons" />
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
   <link href="assets/css/material-kit.css?v=2.0.5" rel="stylesheet" />
@@ -115,7 +199,7 @@ $query = mysqli_query($con,"insert into campus (Name,`College City`,`College Nam
         //Storing values in database
 
         if ($query) {
-            echo '<span id="success">Form Submitted Successfully.<br>We will be in touch with you soon.</span>';
+            echo '<span id="success">Form Submitted Successfully.<br>We will be in touch with you soon.</span><br><a href="/" style="text-align:center; font-size:25px">Go Home</a>';
         } else {
             echo '<span>Form Submission Failed.</span>';
         }
