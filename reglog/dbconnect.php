86066e014bef094ee2148e2cba886db5ef4283b7
root <anillakshya6@gmail.com>
2019-10-19 17:17:02 +0530

    reg and login

--- reglog/dbconnect.php
+++ reglog/dbconnect.php
@@ -2,7 +2,7 @@
   $db_host = "localhost:3306";
   $db_username = "ecellvnit";
   $db_pass = "ECELL@123";
-  $db_name = "ca";
+  $db_name = "ca19";
 
   $con = mysqli_connect("$db_host","$db_username","$db_pass") or die ("could not connect to mysql");
   mysqli_select_db($con,$db_name) or die ("no database");
--- reglog/register.php
+++ reglog/register.php
@@ -1,9 +1,10 @@
 <?php
-
+  error_reporting(E_ALL);
+  ini_set('display_errors', '1');
 
 if(isset($_POST['register'])){
 
-  require "../dbconnect/connect_to_signups.php";
+  require "dbconnect.php";
 
     $Name = $con->real_escape_string($_POST['Name']);
     $CollegeCity = $con->real_escape_string($_POST['CollegeCity']);
@@ -14,41 +15,42 @@ if(isset($_POST['register'])){
     $CurrentAddress = $con->real_escape_string($_POST['CurrentAddress']);
     $CurrentPincode = $con->real_escape_string($_POST['CurrentPincode']);
     $MobileNumber = $con->real_escape_string($_POST['MobileNumber']);
-    $WhatsappNumber = $con->real_escape_string($_POST['WhatsappNumber']);
+    // $WhatsappNumber = $con->real_escape_string($_POST['WhatsappNumber']);
     $Email = $con->real_escape_string($_POST['Email']);
-    $Motive = $con->real_escape_string($_POST['Motive']);
-    $Experience = $con->real_escape_string($_POST['Experience']);
-    $skills = $con->real_escape_string($_POST['skills']);
-    $Approach = $con->real_escape_string($_POST['Approach']);
-    $Requirements = $con->real_escape_string($_POST['Requirements']);
+    // $Motive = $con->real_escape_string($_POST['Motive']);
+    // $Experience = $con->real_escape_string($_POST['Experience']);
+    // $skills = $con->real_escape_string($_POST['skills']);
+    // $Approach = $con->real_escape_string($_POST['Approach']);
+    // $Requirements = $con->real_escape_string($_POST['Requirements']);
+
+    $Password = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789';
+    $Password = str_shuffle($Password);
+    $Password = substr($Password, 0, 8);
 
     // error_reporting(E_ERROR | E_PARSE);
     // mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-    $skills_string = implode(', ', $_POST['skills']);
+    // $skills_string = implode(', ', $_POST['skills']);
 
-    $create_table1 = mysqli_query($con, "CREATE TABLE IF NOT EXISTS CA('Name' varchar(255),
-                                                                      'College City' varchar(255),
-                                                                      'College Name' varchar(255),
-                                                                      'College Strength' varchar(255),
-                                                                      'College Address' varchar(255),
-                                                                      'College Pincode' varchar(255),
-                                                                      'Current Address' varchar(255),
-                                                                      'Current Pincode' varchar(255),
-                                                                      'Mobile Number' varchar(255),
-                                                                      'Whatsapp Number' varchar(255),
-                                                                      'Email' varchar(255),
-                                                                      'Motive' varchar(255),
-                                                                      'Experience' varchar(255),
-                                                                      'Skills' varchar(255),
-                                                                      'Approach' varchar(255),
-                                                                      'Arrangements' varchar(255),
-                                                                      'Password' varchar(255)
-                                                                    )");
+    $create_table1 = mysqli_query($con, "CREATE TABLE IF NOT EXISTS CA(ID INT(6) AUTO_INCREMENT PRIMARY KEY,
+                                                                      Name varchar(255),
+                                                                      CollegeCity varchar(255),
+                                                                      CollegeName varchar(255),
+                                                                      CollegeStrength varchar(255),
+                                                                      CollegeAddress varchar(255),
+                                                                      CollegePincode varchar(255),
+                                                                      CurrentAddress varchar(255),
+                                                                      CurrentPincode varchar(255),
+                                                                      MobileNumber varchar(255),
+                                                                      Email varchar(255),
+                                                                      Password varchar(255) )");
 
-    $create_table2 = mysqli_query($con, "CREATE TABLE IF NOT EXISTS CA('Name' varchar(255))");
-    $insert = mysqli_query($con,"INSERT INTO CA ('Name',`College City`,`College Name`,`College Strength`,`College Address`,`College Pincode`,`Current Address`,`Current Pincode`,`Mobile Number`,`Whatsapp Number`,'Email','Motive','Experience','Skills','Approach','Arrangements')
-                                VALUES('$Name','$CollegeCity','$CollegeName','$CollegeStrength','$CollegeAddress','$CollegePincode','$CurrentAddress','$CurrentPincode','$MobileNumber','$WhatsappNumber','$Email','$Motive','$Experience','$skills_string','$Approach','$Requirements')");
+    $create_table2 = mysqli_query($con, "CREATE TABLE IF NOT EXISTS $Name(ID INT(6) AUTO_INCREMENT PRIMARY KEY,
+                                                                          Task varchar(255),
+                                                                          Points varchar(255) )");
+
+    $insert = mysqli_query($con,"INSERT INTO CA(Name,CollegeCity,CollegeName,CollegeStrength,CollegeAddress,CollegePincode,CurrentAddress,CurrentPincode,MobileNumber,Email,Password)
+                                VALUES('$Name','$CollegeCity','$CollegeName','$CollegeStrength','$CollegeAddress','$CollegePincode','$CurrentAddress','$CurrentPincode','$MobileNumber','$Email','$Password')");
 
 
 
@@ -107,20 +109,23 @@ if(isset($_POST['register'])){
                     <h3><b>Hello '.$Name.',</b></h3>
 
 
-                    <p>Thank You for registering! You are now a part of Central India’s Biggest Entrepreneurship Summit.</p>
+                    <p>Thank You for registering! You are now a part of one of the India’s Biggest Entrepreneurship Summit.</p>
                     <div>
                         <p>We hope this mail finds you in the best of your health and cheerful spirits. We are well pleased to have you on board for this program.</p>
 
 
                         <p>
-                      To keep you updated, all the relevant details will be e-mailed to you very shortly.
+                      To keep you updated, all the relevant details will be e-mailed to you very shortly.<br>
+                      Use these details to login to your dashboard:<br>
+                      Username: '. $Email .'<br>
+                      Password: '. $Password .'
                             Over this month, you will get access to plenty of valuable resources, which will help you guide your way through this program.<br>
                       For queries and in case of any difficulty, feel free to contact us.<br>
 
                     </p>
                         <p>
                             With warm regards,<br>
-                            Anushree Rungta<br>
+                            Gourav Routray<br>
                             Core-Coordinator, Ecell VNIT
                         </p>
 
@@ -161,8 +166,8 @@ if(isset($_POST['register'])){
 
 <head>
   <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="assets/img/favicon.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
     Campus Ambassador Programme
@@ -173,7 +178,7 @@ if(isset($_POST['register'])){
   <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
-  <link href="assets/css/material-kit.css?v=2.0.5" rel="stylesheet" />
+  <link href="../assets/css/material-kit.css?v=2.0.5" rel="stylesheet" />
 </head>
 
 <body>
@@ -182,7 +187,7 @@ if(isset($_POST['register'])){
     <div class="container">
       <div class="navbar-translate">
         <a class="navbar-brand" href="https://www.ecellvnit.org/">
-          <img src="assets/img/conso.png" width="150px"> </a>
+          <img src="../assets/img/conso.png" width="150px"> </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="sr-only">Toggle navigation</span>
           <span class="navbar-toggler-icon"></span>
@@ -223,7 +228,7 @@ if(isset($_POST['register'])){
 
         <div class="row">
             <div class="col-sm-12 text" style="display:flex; justify-content:center">
-                <img src="assets/img/ca_head.jpg" width="100%">
+                <img src="../assets/img/ca_head.jpg" width="100%">
             </div>
         </div>
 
@@ -234,19 +239,19 @@ if(isset($_POST['register'])){
         <?php
         //Storing values in database
 
-        if ($query) {
+        if ($insert) {
             echo '<span id="success">Form Submitted Successfully.<br>We will be in touch with you soon.</span><br><a href="/" style="text-align:center; font-size:25px">Go Home</a>';
         } else {
-            echo '<span>Form Submission Failed.</span>';
+            echo(mysqli_error($con));
         }
 
 
-        if (!$query) {
+        if (!$insert) {
             echo '<a href="/" style="text-align:center; font-size:25px"> Go Back</a>';
         }
         echo '</p>';
 
-        if ($query) {
+        if ($insert) {
 
             echo'<div style="width:100%">
 
@@ -265,7 +270,7 @@ if(isset($_POST['register'])){
     }
 
     </style>
-    <img src="assets/img/success.jpg" alt="" class="success" >
+    <img src="../assets/img/success.jpg" alt="" class="success" >
 </div>';
 
         }
@@ -280,21 +285,21 @@ if(isset($_POST['register'])){
 
     </div>
 </div>
-<?php include("footer.php") ?>
+<?php include("../includes/footer.php") ?>
 
 <!--   Core JS Files   -->
-  <script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
-  <script src="assets/js/core/popper.min.js" type="text/javascript"></script>
-  <script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
-  <script src="assets/js/plugins/moment.min.js"></script>
+  <script src="../assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="../assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+  <script src="../assets/js/plugins/moment.min.js"></script>
   <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
-  <script src="assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+  <script src="../assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
   <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-  <script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <script src="../assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
   <!--  Google Maps Plugin    -->
   <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
   <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
-  <script src="assets/js/material-kit.js?v=2.0.5" type="text/javascript"></script>
+  <script src="../assets/js/material-kit.js?v=2.0.5" type="text/javascript"></script>
 </body>
 
 </html>
