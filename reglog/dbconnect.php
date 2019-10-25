b3e8634fbce6c39b37c9b05793847c1a94885756
JituR101 <jitendra98rahangdale@gmail.com>
2019-10-25 12:31:55 +0530

    dab connect

--- reglog/dbconnect.php
+++ reglog/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
   $db_host = "localhost:3306";
   $db_username = "vipul3";
-  $db_pass = "Ecellvnit@123";
+  $db_pass = "Ecellvnit123@";
   $db_name = "CampusAmbassador";
 
   $con = mysqli_connect("$db_host","$db_username","$db_pass") or die ("could not connect to mysql");
