15955d8885051e62c304c0de7525473ab70f8adb
JituR101 <jitendra98rahangdale@gmail.com>
2019-10-25 13:22:48 +0530

    head and footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-<footer style="background-color: #212529;">
-                    <!-- End Links -->
-                    <div class="container" style="padding: 60px 30px">
-                        <div class="row">
-                            <div class="col-sm-12 col-md-6">
-                                    <a href="/" style="margin-left: 20px;">
-                                        <img class="g-width-100--xs g-height-auto--xs" src="assets/img/conso1.png" width="150px" alt="Ecell Logo">
-                                    </a>
-                                <br><br>
-                                <a class="s-header__action-link" style="color:#fff; margin:0px 4px;" href=" https://m.facebook.com/vnitecell/" target="_blank">
-                                    <i class="g-padding-r-5--xs fa fa-facebook"></i>
-                                    <span class="g-display-none--xs g-display-inline-block--sm"> </span>
-                                </a>
-                                <a class="s-header__action-link" style="color:#fff; margin:0px 4px;" href="https://twitter.com/ecell_vnit" target="_blank">
-                                    <i class="g-padding-r-5--xs fa fa-twitter"></i>
-                                    <span class="g-display-none--xs g-display-inline-block--sm"> </span>
-                                </a>
-                                <a class="s-header__action-link" style="color:#fff; margin:0px 4px;" href=" https://www.instagram.com/ecellvnit/" target="_blank">
-                                    <i class="g-padding-r-5--xs fa fa-instagram"></i>
-                                    <span class="g-display-none--xs g-display-inline-block--sm"> </span>
-                                </a>
-                                <a class="s-header__action-link" style="color:#fff; margin:0px 4px;" href=" https://www.linkedin.com/company/6615520/" target="_blank">
-                                    <i class="g-padding-r-5--xs fa fa-linkedin"></i>
-                                    <span class="g-display-none--xs g-display-inline-block--sm"> </span>
-                                </a>
-                                <a class="s-header__action-link" style="color:#fff; margin:0px 4px;"  href=" https://www.youtube.com/channel/UC0KNOmODhqLcEpcfN9qEsIQ" target="_blank">
-                                    <i class="g-padding-r-5--xs fa fa-youtube"></i>
-                                    <span class="g-display-none--xs g-display-inline-block--sm"> </span>
-                                </a>
-                            </div>
-                            <div class="col-sm-12 col-md-6">
-                                    <p style="color:#fff">Contact:<b><a href="https://ecellvnit.org/team.php"> Team</a></b></p>
-                                    <p style="color:#fff">For Any Queries Contact:<br><b>contactus@ecellvnit.org<b></b></b></p><b><b>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Copyright -->
-                </footer>
-=======
 <footer class="g-bg-color--dark">
     <!-- Copyright -->
     <div class="container g-padding-y-20--xs">
@@ -80,4 +40,3 @@
     </div>
     <!-- End Copyright -->
 </footer>
->>>>>>> 4d6805056ec14d1817c6e1a2abfca283cb363ae4
--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,4 @@
 <!-- Begin Head -->
-<<<<<<< HEAD
     <head>
 
 
@@ -44,27 +43,3 @@
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
 
     </head>
-=======
-<head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1">
-  <meta name="google-site-verification" content="gXeMBX--H7xSFeawu77WYuSIYyuZQdkI4YQQnV0Hf_Y" />
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="keywords" content="Ecell, vnit, E-cell VNIT, entrepreneurship cell, consortium 20, 2020, consortium'20, campus Ambassador, program, internship" />
-  <meta name="description" content="Consortium's Campus Ambassador Program is a campus connect program that aims to form a network of like-minded students across India. The program facilitates and nurtures future leaders of the country and contributes to their career growth. We want to empower and groom select students across India who will serve as experts in their respective institutions.">
-  <meta name="author" content="E-Cell VNIT">
-  <title>Campus Ambassador Program | E-Cell VNIT</title>
-
-  <!--     Fonts and icons     -->
-  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/ecell.png">
-  <!-- <link rel="icon" type="image/png" href="assets/img/favicon.png"> -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Material+Icons" />
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-  <!-- CSS Files -->
-  <link href="assets/css/material-kit.css?v=0.0.1" rel="stylesheet" />
-  <link href="css/global/global.css" rel="stylesheet" />
-</head>
->>>>>>> 4d6805056ec14d1817c6e1a2abfca283cb363ae4
-    <!-- End Head -->
