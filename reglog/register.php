b4bb30603842c646f65eb5b9c34de39f96536d8b
JituR101 <jitendra98rahangdale@gmail.com>
2019-11-15 17:56:18 +0530

    email changes

--- reglog/register.php
+++ reglog/register.php
@@ -65,7 +65,7 @@
       //Username to use for SMTP authentication - use full email address for gmail
       $mail->Username = 'contact@ecellvnit.org';
       //Password to use for SMTP authentication
-      $mail->Password = 'Entreprenuers1999';
+      $mail->Password = 'Entrepreneurs1999';
       //Set who the message is to be sent from
       $mail->setFrom('contact@ecellvnit.org');
       //Set who the message is to be sent to
