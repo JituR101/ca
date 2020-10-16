d2b2b3d0442e84fd419b4ad1a14607051052068a
Piyush <Piyusshh@users.noreply.github.com>
2020-10-16 18:21:23 +0530

    real escape string

--- store.php
+++ store.php
@@ -17,15 +17,15 @@ if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }	 
  
- $Name=$_POST['Name'];
- $email=$_POST['Email'];
- $collegeName=$_POST['CollegeName'];
- $collegeCity = $_POST['CollegeCity'];
- $collegeStrength = $_POST['CollegeStrength'];
- $collegePincode = $_POST['CollegePincode'];
- $collegeAddress = $_POST['CollegeAddress'];
- $mobileNumber = $_POST['MobileNumber'];
- $whatsappNumber = $_POST['WhatsappNumber'];
+ $Name = $conn -> real_escape_string($_POST['Name']);
+ $email = $conn -> real_escape_string($_POST['Email']);
+ $collegeName = $conn -> real_escape_string($_POST['CollegeName']);
+ $collegeCity = $conn -> real_escape_string($_POST['CollegeCity']);
+ $collegeStrength = $conn -> real_escape_string($_POST['CollegeStrength']);
+ $collegePincode = $conn -> real_escape_string($_POST['CollegePincode']);
+ $collegeAddress = $conn -> real_escape_string($_POST['CollegeAddress']);
+ $mobileNumber = $conn -> real_escape_string($_POST['MobileNumber']);
+ $whatsappNumber = $conn -> real_escape_string($_POST['WhatsappNumber']);
   $_SESSION['name']=$Name;
    $sql = "INSERT INTO cap (name, email, collegeName, collegeCity, collegeStrength, pincode, collegeAdd, mobile, whatsapp) VALUES 
    ('$Name', '$email', '$collegeName','$collegeCity', '$collegeStrength', '$collegePincode', '$collegeAddress', '$mobileNumber', '$whatsappNumber' )";   
