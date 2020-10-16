a260cd8cd975a2bc506acb5bc7fa4cf58118ad06
Piyush <Piyusshh@users.noreply.github.com>
2020-10-16 23:12:35 +0530

    email format corrected

--- mailing.php
+++ mailing.php
@@ -12,7 +12,6 @@ function htmlMail($t, $sub, $name, $teamname, $event){
     $html = "<!DOCTYPE html>
               <html>
                   <head>
-                  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1">
                       <style>
                           li{
                               padding:10px;
